d33ca43271d75453423e5e5ea6ab394753f65420
alceil <ashishshajisrampickal@gmail.com>
2023-03-16 23:21:05 +0530

    Add Password field

--- src/pages/auth/SignUp/SignUp.tsx
+++ src/pages/auth/SignUp/SignUp.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from './SignUp.module.css'
 import {HiOutlineMail} from 'react-icons/hi'
+import {MdOutlineLockOpen} from 'react-icons/md'
 type Props = {}
 
 const SignUp = (props: Props) => {
@@ -16,6 +17,14 @@ const SignUp = (props: Props) => {
   name="text" 
   placeholder="      Email" 
   type="email"/>
+</div>
+<div style={{position: 'relative', display: 'inline-block'}}>
+<MdOutlineLockOpen style={{position: 'absolute', left: 5, top: 14, width: 20, height: 20,color:'grey'}}/>
+<input 
+  className={style.emailInput} 
+  name="text" 
+  placeholder="      Password" 
+  type="password"/>
 </div>
 <button className={style.click}>Next</button>
       </div>
