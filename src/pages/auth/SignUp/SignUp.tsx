b46f11930689dd3d4826e59a7cf941d0de4dfd5e
alceil <ashishshajisrampickal@gmail.com>
2023-03-16 22:58:21 +0530

    Added Sigup button

--- src/pages/auth/SignUp/SignUp.tsx
+++ src/pages/auth/SignUp/SignUp.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from './SignUp.module.css'
+import {HiOutlineMail} from 'react-icons/hi'
 type Props = {}
 
 const SignUp = (props: Props) => {
@@ -8,11 +9,15 @@ const SignUp = (props: Props) => {
       <div className={style.SignUpBox}>
 <h5>Sign in</h5>
 <p>Use your trio calls account</p>
+<div style={{position: 'relative', display: 'inline-block'}}>
+<HiOutlineMail style={{position: 'absolute', left: 5, top: 14, width: 20, height: 20,color:'grey'}}/>
 <input 
   className={style.emailInput} 
   name="text" 
-  placeholder="Email" 
+  placeholder="      Email" 
   type="email"/>
+</div>
+<button className={style.click}>Next</button>
       </div>
     </div>
   )
