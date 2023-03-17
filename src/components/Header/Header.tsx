83b9d4aefe9b910d9bd407e56b29ee20447ad38d
alceil <ashishshajisrampickal@gmail.com>
2023-03-17 20:05:28 +0530

    Added header buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Logo from '../Logo'
 import style from './Header.module.css'
+import {MdBrightness4,MdBrightness7} from 'react-icons/md'
+
 const Header = () => {
+  const [useDark, setUseDark] = React.useState(false);
   return (
    <header className={style.nav_container}>
     <div  className={style.logo_container} >
@@ -12,7 +15,10 @@ className={style.header_text}
   Ymeet
 </h6>
     </div>
-
+    <div className={style.button_group}>
+    {!useDark ? <MdBrightness4 style={{color:'grey'}} size={25}  /> : <MdBrightness7 />}
+<button className={style.displayBtn}>Alceil</button>
+    </div>
    </header>
   )
 }
