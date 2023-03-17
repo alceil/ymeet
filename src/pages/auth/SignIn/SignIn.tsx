4cfdf2ef3b874a5be0afba557f0d43cb4c18140a
alceil <ashishshajisrampickal@gmail.com>
2023-03-17 12:19:44 +0530

    Added Signup page

--- src/pages/auth/SignIn/SignIn.tsx
+++ src/pages/auth/SignIn/SignIn.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import style from './SignIn.module.css'
+import {HiOutlineMail} from 'react-icons/hi'
+import {MdOutlineLockOpen} from 'react-icons/md'
 const SignIn: React.FC = () => {
   return (
-    <div>SignIn</div>
+    <div className={style.Container}>
+      <div className={style.SignUpBox}>
+<h5>Sign in</h5>
+<p>Use your trio calls account</p>
+<div style={{position: 'relative', display: 'inline-block'}}>
+<HiOutlineMail style={{position: 'absolute', left: 5, top: 14, width: 20, height: 20,color:'grey'}}/>
+<input 
+  className={style.emailInput} 
+  name="text" 
+  placeholder="      Email" 
+  type="email"/>
+</div>
+<div style={{position: 'relative', display: 'inline-block'}}>
+<MdOutlineLockOpen style={{position: 'absolute', left: 5, top: 14, width: 20, height: 20,color:'grey'}}/>
+<input 
+  className={style.emailInput} 
+  name="text" 
+  placeholder="      Password" 
+  type="password"/>
+</div>
+<button className={style.click}>Login</button>
+<p>Create new account</p>
+      </div>
+    </div>
   )
 }
 
--- src/pages/auth/SignUp/SignUp.tsx
+++ src/pages/auth/SignUp/SignUp.tsx
@@ -2,14 +2,23 @@ import React from 'react'
 import style from './SignUp.module.css'
 import {HiOutlineMail} from 'react-icons/hi'
 import {MdOutlineLockOpen} from 'react-icons/md'
+import {BsPerson} from 'react-icons/bs'
+
 type Props = {}
 
 const SignUp = (props: Props) => {
   return (
     <div className={style.Container}>
       <div className={style.SignUpBox}>
-<h5>Sign in</h5>
-<p>Use your trio calls account</p>
+<p>Create your team account</p>
+<div style={{position: 'relative', display: 'inline-block'}}>
+<BsPerson style={{position: 'absolute', left: 5, top: 14, width: 20, height: 20,color:'grey'}}/>
+<input 
+  className={style.emailInput} 
+  name="text" 
+  placeholder="      Display Name" 
+  type="email"/>
+</div>
 <div style={{position: 'relative', display: 'inline-block'}}>
 <HiOutlineMail style={{position: 'absolute', left: 5, top: 14, width: 20, height: 20,color:'grey'}}/>
 <input 
@@ -26,7 +35,8 @@ const SignUp = (props: Props) => {
   placeholder="      Password" 
   type="password"/>
 </div>
-<button className={style.click}>Next</button>
+<button className={style.click}>Register Now</button>
+<p>Already have an account?Sign in</p>
       </div>
     </div>
   )
