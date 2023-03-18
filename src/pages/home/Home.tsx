d13e6cad4600ae902e9e4c603ee2c56586147a72
alceil <ashishshajisrampickal@gmail.com>
2023-03-18 15:02:55 +0530

    Added chatButton

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import JoinMeet from './components/JoinMeet/JoinMeet'
-
+import {BsChatLeftText} from 'react-icons/bs'
 const Home = () => {
   return (
 <>
@@ -19,6 +19,28 @@ const Home = () => {
     src="https://www.gstatic.com/meet/user_edu_brady_bunch_light_81fa864771e5c1dd6c75abe020c61345.svg"
   />
 <JoinMeet/>
+
+</div>
+
+<div style={
+  {
+    display:'flex',
+    justifyContent:'end'
+    
+  }}>
+<button style={{
+  display:'flex',
+  alignItems:'center',
+  marginRight:10,
+borderRadius:50,
+padding:15,
+border:'none',
+color:'#fff',
+boxShadow:'0px 0px 30px 1px #c2c2c2',
+backgroundColor:'#4527a0'
+}}>
+<BsChatLeftText size={20}/>
+</button>
 
 </div>
 
