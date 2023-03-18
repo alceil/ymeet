6e6785fec30678c01b66b4d10ed38bcd1d7dc6a8
alceil <ashishshajisrampickal@gmail.com>
2023-03-18 16:52:31 +0530

    initiliased chatheader component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import SignUp from './pages/auth/SignUp/SignUp';
+import Chat from './pages/chat/Chat';
 import Home from './pages/home/Home';
 
 function App() {
   return (
-<Home/>
+<Chat/>
   );
 }
 
--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Header from '../../components/Header/Header'
+import style from './styles.module.css'
 const Chat = () => {
   return (
 <>
+<Header/>
 
 </>
   )
