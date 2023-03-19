27b1c2012be296f656e7b11f64cceee00add53a0
alceil <ashishshajisrampickal@gmail.com>
2023-03-19 22:34:10 +0530

    Added avatar

--- src/pages/chat/components/ChatParticipants.tsx
+++ src/pages/chat/components/ChatParticipants.tsx
@@ -3,7 +3,7 @@ import style from './styles/ChatParticipants.module.css'
 import {MdSearch} from 'react-icons/md'
 const ChatParticipants = () => {
   return (
-    <div>
+    <div className={style.chatparticipants}>
         <div style={{position: 'relative', display: 'inline-block',margin:10}}>
 <MdSearch style={{position: 'absolute', left: 5, top: 11, width: 20, height: 20,color:'grey'}}/>
 <input 
@@ -12,7 +12,13 @@ const ChatParticipants = () => {
   placeholder="      Search" 
   type="search"/>
 </div>
-        
+<div className={style.list_card}>
+<div className={style.avatar}>
+    I
+    </div> 
+ 
+</div>
+  
 </div>
   )
 }
