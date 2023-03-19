e14a0694f498cf11479655a6979a5a5d502736f7
alceil <ashishshajisrampickal@gmail.com>
2023-03-19 23:10:03 +0530

    Added chat image

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -7,9 +7,19 @@ const Chat = () => {
   return (
 <>
 <Header/>
-<div className={style.SignUpBox}>
+<div className={style.chat_container}>
 <ChatHeader/>
+<div>
 <ChatParticipants/>
+<img
+                    alt="Start new chat"
+                    width={180}
+                    style={{ alignSelf: "center", margin: 20 }}
+                    src={
+                      "https://www.gstatic.com/meet/user_edu_get_a_link_light_90698cd7b4ca04d3005c962a3756c42d.svg"
+                    }
+                  />
+</div>
 </div>
 </>
   )
