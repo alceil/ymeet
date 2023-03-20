83c4bc6e11ee7b9ca71bb0475a3666c3c274fbfd
alceil <ashishshajisrampickal@gmail.com>
2023-03-20 18:56:10 +0530

    Added Ready component

--- src/pages/meet/waitRoom/components/ReadyComponent.tsx
+++ src/pages/meet/waitRoom/components/ReadyComponent.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import style from './styles/ReadyComponent.module.css'
 const ReadyComponent = () => {
   return (
-    <div>ReadyComponent</div>
+    <div className={style.joinmeet_right}>
+        <h2 className={style.joinmeet_title}>Ready to Join?</h2>
+        <button className={style.joinmeet_btn}>Join Now</button>
+    </div>
   )
 }
 
