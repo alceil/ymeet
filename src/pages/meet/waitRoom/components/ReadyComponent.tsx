9a72e496a81c9578253e9014b68209b8433bad77
alceil <ashishshajisrampickal@gmail.com>
2023-03-20 19:08:07 +0530

    aligned readycomponent

--- src/pages/meet/waitRoom/components/ReadyComponent.tsx
+++ src/pages/meet/waitRoom/components/ReadyComponent.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import style from './styles/ReadyComponent.module.css'
+import {MdOutlineVoiceChat} from 'react-icons/md'
 const ReadyComponent = () => {
   return (
     <div className={style.joinmeet_right}>
         <h2 className={style.joinmeet_title}>Ready to Join?</h2>
-        <button className={style.joinmeet_btn}>Join Now</button>
+        <button className={style.joinmeet_btn}>
+            <MdOutlineVoiceChat size={18}/>
+           <span style={{marginRight:'10px'}} >
+           Join Now
+            </span>
+            
+            </button>
     </div>
   )
 }
