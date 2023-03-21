f735e8975af442eb1587b4771d233422dca18f21
alceil <ashishshajisrampickal@gmail.com>
2023-03-21 15:32:20 +0530

    intialised sidepanel component

---
+++ src/pages/meet/dashboard/components/SidePanel.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from './styles/SidePanel.module.css'
+import {
+    MdInfoOutline
+} from 'react-icons/md'
+const SidePanel = () => {
+  return (
+    <div className={style.sidepanel_container}>
+<MdInfoOutline/>
+    </div>
+  )
+}
+
+export default SidePanel
