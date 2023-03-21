faf08a37b360f5bb8841865aa21d4cfb0420d98e
alceil <ashishshajisrampickal@gmail.com>
2023-03-21 11:38:43 +0530

    intialised Video component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import SignUp from './pages/auth/SignUp/SignUp';
 import Chat from './pages/chat/Chat';
 import Home from './pages/home/Home';
+import Meet from './pages/meet/dashboard/Meet';
 import WaitRoom from './pages/meet/waitRoom/WaitRoom';
 
 function App() {
   return (
-<WaitRoom/>
+<Meet/>
 
 );
 }
---
+++ src/pages/meet/dashboard/Meet.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './Meet.module.css';
+
+const Meet = () => {
+  return (
+    <div className={style.meet_container}>
+   <div className={style.video_box}>
+   </div>
+    </div>
+  )
+}
+
+export default Meet
