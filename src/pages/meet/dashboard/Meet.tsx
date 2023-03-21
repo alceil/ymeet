a91c09715d1d62d7f1425d6dc422ae61a607b7b0
alceil <ashishshajisrampickal@gmail.com>
2023-03-21 19:41:54 +0530

    Added meeting card

--- src/pages/meet/dashboard/Meet.tsx
+++ src/pages/meet/dashboard/Meet.tsx
@@ -7,7 +7,12 @@ import style from './Meet.module.css';
 const Meet = () => {
   return (
     <div className={style.meet_container}>
-<Video/>
+      <div style={{display:'flex'}}>
+      <Video/>
+<div className={style.meeting_card}>
+<h6>Meet</h6>
+</div>
+      </div>
 <div className={style.vc_controllers}>
 <h2 style={{color:'#0000008A',fontWeight:'bold'}}>
   2:45pm | i6fl-k5cd-gvom
