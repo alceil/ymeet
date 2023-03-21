1203b03da8773c4264a274b2ec521505c1bd3cae
alceil <ashishshajisrampickal@gmail.com>
2023-03-21 10:35:57 +0530

    Added vc options  card

--- src/pages/meet/waitRoom/components/Preview.tsx
+++ src/pages/meet/waitRoom/components/Preview.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import style from './styles/Preview.module.css'
+import {IoMdMic} from 'react-icons/io'
+import {MdOutlineVideocam} from 'react-icons/md'
 const Preview = () => {
   return (
     <div className={style.video_box}>
+      <div className={style.vc_options}>
+       <IoMdMic/> 
+       <MdOutlineVideocam/>
+
+      </div>
     </div>
   )
 }
