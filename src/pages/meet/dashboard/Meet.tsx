d61a63828020a7196374d4a580b4be254b82a96a
alceil <ashishshajisrampickal@gmail.com>
2023-03-21 14:51:22 +0530

    initialised Controller

--- src/pages/meet/dashboard/Meet.tsx
+++ src/pages/meet/dashboard/Meet.tsx
@@ -6,6 +6,11 @@ const Meet = () => {
   return (
     <div className={style.meet_container}>
 <Video/>
+<div className={style.vc_controllers}>
+<h2 style={{color:'#0000008A',fontWeight:'bold'}}>
+  2:45pm | i6fl-k5cd-gvom
+</h2>
+</div>
     </div>
   )
 }
