0b829676efaebb47f72ccaa1209fcb19d5f6bc16
alceil <ashishshajisrampickal@gmail.com>
2023-03-23 14:02:05 +0530

    Added meetingcard with sidepanel

--- src/pages/meet/dashboard/Meet.tsx
+++ src/pages/meet/dashboard/Meet.tsx
@@ -3,29 +3,13 @@ import Controller from './components/Controller';
 import SidePanel from './components/SidePanel';
 import Video from './components/Video';
 import style from './Meet.module.css';
-import { MdClose,MdOutlineFileCopy} from 'react-icons/md'
 const Meet = () => {
   const [isOpen,setIsOpen] = useState(false);
   return (
     <div className={style.meet_container}>
       <div style={{display:'flex'}}>
       <Video open={isOpen}/>
-  <div className={`${style.meeting_card} ${isOpen?`${style.transition}`:` `}`}>
-  <div className={style.meeting_card_top}>
-  <h3>Meeting Details</h3>
-  <MdClose size={20} style={{color:'#0000008A'}}/>
-  </div>
-  <div>
-    <p>Joining info</p>
-    <span style={{color:'#0000008A'}}>
-    https://trio-calls.vercel.app/i6fl-k5cd-gvom
-    </span>
-    <button className={style.joincard_btn}>
-    <MdOutlineFileCopy size={20}/>
-<span style={{marginLeft:'10px'}}>Copy joining info</span>
-    </button>
-  </div>
-</div>
+  
 
       </div>
 <div className={style.vc_controllers}>
@@ -33,8 +17,9 @@ const Meet = () => {
   2:45pm | i6fl-k5cd-gvom
 </h2>
 <Controller/>
-<SidePanel/>
 </div>
+<SidePanel open={isOpen} setOpen={setIsOpen}/>
+
     </div>
   )
 }
--- src/pages/meet/dashboard/components/SidePanel.tsx
+++ src/pages/meet/dashboard/components/SidePanel.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
 import style from './styles/SidePanel.module.css'
+import { MdClose,MdOutlineFileCopy} from 'react-icons/md'
+
 import {
     MdInfoOutline,
     MdOutlinePeopleOutline,
@@ -7,15 +8,37 @@ import {
     MdLockOpen
 } from 'react-icons/md'
 import { BsChatLeftText } from "react-icons/bs";
-const SidePanel = () => {
+const SidePanel:React.FC< {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}> = ({ open, setOpen }) => {
   return (
-    <div className={style.sidepanel_container}>
+    <div>
+      <div className={`${style.meeting_card} ${open?`${style.transition}`:` `}`}>
+  <div className={style.meeting_card_top}>
+  <h3>Meeting Details</h3>
+  <MdClose size={20} style={{color:'#0000008A'}}/>
+  </div>
+  <div>
+    <p>Joining info</p>
+    <span style={{color:'#0000008A'}}>
+    https://trio-calls.vercel.app/i6fl-k5cd-gvom
+    </span>
+    <button className={style.joincard_btn}>
+    <MdOutlineFileCopy size={20}/>
+<span style={{marginLeft:'10px'}}>Copy joining info</span>
+    </button>
+  </div>
+</div>
+<div className={style.sidepanel_container}>
 <MdInfoOutline size={24}/>
 <MdOutlinePeopleOutline size={24}/>
 <BsChatLeftText size={24}/>
 <MdOutlineAssessment size={24}/>
 <MdLockOpen size={24}/>
     </div>
+    </div>
+
   )
 }
 
