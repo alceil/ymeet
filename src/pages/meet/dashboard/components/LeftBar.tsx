8d20b3f94f466c5516dcabb4386943286dc7a04d
alceil <ashishshajisrampickal@gmail.com>
2023-03-23 14:54:04 +0530

    Added LeftBar component

---
+++ src/pages/meet/dashboard/components/LeftBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeftBar = () => {
+  return (
+<h2 style={{position:'absolute',bottom:'24px',left:'10px',color:'#0000008A',fontWeight:'bold'}}>
+  2:45pm | i6fl-k5cd-gvom
+</h2>
+  )
+}
+
+export default LeftBar
