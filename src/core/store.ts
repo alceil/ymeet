47850cfc8c450a3419a9591921fa9ff059429631
alceil <ashishshajisrampickal@gmail.com>
2023-03-28 17:00:37 +0530

    configured store

---
+++ src/core/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./reducers/auth";
+
+
+const store = configureStore({
+  reducer: {
+    authReducer
+  },
+  devTools: true,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
