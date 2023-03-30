3af90703112ce8d32f2242cff4a32bc91397ed2f
alceil <ashishshajisrampickal@gmail.com>
2023-03-30 16:24:36 +0530

    Added ReduxProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import loadable from '@loadable/component'
 import { LinearProgress } from '@mui/material';
+import { Provider as ReduxProvider } from "react-redux";
 import { SnackbarProvider } from "notistack";
+import store from './core/store';
 export const Routes = loadable(() => import('./pages'), {
   fallback: <LinearProgress />,
 });
 function App() {
   return (
+    <ReduxProvider store={store}>
     <SnackbarProvider preventDuplicate maxSnack={3}>
 <Routes/>
     </SnackbarProvider>
+    </ReduxProvider>
+
 );
 }
 
