8e8721bc2fc32597ff8b9ff29b1a00a0077a055c
alceil <ashishshajisrampickal@gmail.com>
2023-04-03 15:00:00 +0530

    🚀Added preview features

--- src/pages/meet/waitRoom/components/Preview.tsx
+++ src/pages/meet/waitRoom/components/Preview.tsx
@@ -2,9 +2,31 @@ import React from 'react'
 import style from './styles/Preview.module.css'
 import {IoMdMic} from 'react-icons/io'
 import {MdOutlineVideocam} from 'react-icons/md'
+import { useAppDispatch, useAppSelector } from '../../../../core/hooks/redux'
 const Preview = () => {
+  const videoController = React.useRef<HTMLVideoElement>(null);
+  const dispatch = useAppDispatch();
+  const { isAudio, isVideo } = useAppSelector(
+    ({ mediaReducer }) => mediaReducer
+  );
+  React.useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: isVideo, audio: true })
+      .then((stream) => {
+        if (videoController.current) {
+          videoController.current.srcObject = stream;
+        }
+      });
+  }, [isVideo]);
+
+  React.useEffect(() => {
+    if (videoController.current) {
+      videoController.current.muted = !isAudio;
+    }
+  }, [isAudio]);
   return (
     <div className={style.video_box}>
+              <video muted className={style.video_box} ref={videoController} autoPlay />
       <div className={style.vc_options}>
        <IoMdMic size={20} style={{color:'rgba(0, 0, 0, 0.54)'}}/> 
        <MdOutlineVideocam size={20} style={{color:'rgba(0, 0, 0, 0.54)'}}/>
