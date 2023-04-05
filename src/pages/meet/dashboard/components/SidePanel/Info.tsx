52c2f8d7f11acc92ee6320af86fb6a98f2a20bf4
alceil <ashishshajisrampickal@gmail.com>
2023-04-05 10:50:18 +0530

    Added meeting info function

--- src/pages/meet/dashboard/components/SidePanel/Info.tsx
+++ src/pages/meet/dashboard/components/SidePanel/Info.tsx
@@ -1,13 +1,31 @@
+import { useSnackbar } from 'notistack';
 import React from 'react'
 import { MdOutlineFileCopy} from 'react-icons/md'
-const MeetInfo = () => {
+import { CopyToClipboard } from '../../../../../utils/common';
+type Props = {
+  meetLink?: string;
+};
+const MeetInfo:React.FC<Props> = ({ meetLink = window.location.href }) => {
+  const { enqueueSnackbar } = useSnackbar();
+  const handlePress = () => {
+    enqueueSnackbar("Copied meeting link");
+    CopyToClipboard(meetLink);
+  };
   return (
     <div>
     <p>Joining info</p>
     <span style={{color:'#0000008A'}}>
-    https://trio-calls.vercel.app/i6fl-k5cd-gvom
+    {meetLink}
     </span>
-    <button style={{display: 'flex',alignItems: 'center',border: 'none',margin: '10px auto',backgroundColor: '#ffff',color:'#4527a0'}}>
+    <button 
+    style={{display: 'flex',
+    alignItems: 'center',
+    border: 'none',
+    margin: '10px auto',
+    backgroundColor: '#ffff',
+    color:'#4527a0'}}
+    onClick={handlePress}
+    >
     <MdOutlineFileCopy size={20}/>
 <span style={{marginLeft:'10px'}}>Copy joining info</span>
     </button>
