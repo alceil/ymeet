42975ecee72a6b68238cc965366b0f48e0381967
alceil <ashishshajisrampickal@gmail.com>
2023-04-05 17:40:30 +0530

    Added Polls functionality

--- src/pages/meet/dashboard/components/SidePanel/Polls.tsx
+++ src/pages/meet/dashboard/components/SidePanel/Polls.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
 import {MdAdd} from 'react-icons/md'
-const Polls = () => {
+import PollModal from "./Poll/Modal";
+import AddIcon from "@mui/icons-material/Add";
+import { PollType } from './Poll/PollType';
+import PollAlert from "./Poll/Form";
+import { useSocket } from '../../../../../core/hooks/useSocket';
+import { useSnackbar } from 'notistack';
+import { useAppSelector } from '../../../../../core/hooks/redux';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+type Props = {
+  isHost?: boolean;
+};
+
+
+
+const Polls: React.FC<Props> = ({ isHost }) => {
+console.log(isHost)
+  const socketClient = useSocket();
+  const { enqueueSnackbar } = useSnackbar();
+  const [openModal, setOpenModal] = React.useState(false);
+  const poll = useAppSelector((state) => state.meetReducer.poll);
+  const handlePress = () => {
+    setOpenModal(true);
+  };
+
+  const createPoll = React.useCallback((pollData: PollType) => {
+    socketClient.emit("newPoll", pollData);
+    enqueueSnackbar("Poll Published");
+  }, []);
   return (
-    <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-between'}}>
+    <>
+    {/* <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-between'}}>
     <img
               width={"90"}
               alt="Poll Icon"
@@ -23,7 +54,45 @@ const Polls = () => {
               Create a poll
               </span>
             </button>
-    </div>
+    </div> */}
+
+
+
+    <PollModal
+        pollHandler={createPoll}
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+      />
+      {!poll.question ? (
+        <Box py={3} display="flex" justifyContent="center">
+          <img
+            width={"90"}
+            alt="Poll Icon"
+            src="https://avatars.slack-edge.com/2020-05-09/1112549471909_7543dde099089941d3c3_512.png"
+          />
+        </Box>
+      ) : (
+        <PollAlert {...poll} />
+      )}
+      {isHost ? (
+        <Button
+          fullWidth
+          startIcon={<AddIcon />}
+          variant="text"
+          color="secondary"
+          onClick={handlePress}
+        >
+          Create a poll
+        </Button>
+      ) : (
+        !poll.question && (
+          <Typography variant="h5" align="center" color="textSecondary">
+            Polls will appear here
+          </Typography>
+        )
+      )}
+    </>
+
   )
 }
 
