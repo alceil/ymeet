30fc275e28242e78534e073b059dc63f40c6b0e1
alceil <ashishshajisrampickal@gmail.com>
2023-04-05 17:40:55 +0530

    Added after poll component

---
+++ src/pages/meet/dashboard/components/SidePanel/Poll/AfterPoll.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Chip from "@mui/material/Chip";
+// import { BorderLinearProgress } from "../BorderLinearProgress";
+import { PollType } from "./PollType";
+
+export const AfterPoll: React.FC<PollType & { myRes?: string }> = ({
+  question,
+  options,
+  correct,
+  myRes,
+}) => (
+  <Box p={2} borderRadius={3} border="1px solid #e2e2e2">
+    <Box display="flex" justifyContent="space-between">
+      <Typography variant="subtitle1" color="textPrimary">
+        {question}
+      </Typography>
+      <Chip label="Live" color="primary" />
+    </Box>
+    {options.map((opt) => (
+      <>
+        <Box py={1} display="flex" justifyContent="space-between" key={opt}>
+          <Typography variant="body2" color="textSecondary">
+            {opt}
+          </Typography>
+          <Typography variant="caption" color="textSecondary">
+            {opt === myRes ? 1 : 0} vote
+          </Typography>
+        </Box>
+        {/* <BorderLinearProgress
+          variant="determinate"
+          value={opt === correct ? 100 : 0}
+        /> */}
+      </>
+    ))}
+  </Box>
+);
